f72e5fceeaa4d794855ea928a4602f70b3b03da2
RadiKovachevDev <radi.kovachev.dev@gmail.com>
2026-09-23 09:39:19 +0300

    Initialazing]

---
+++ BookShopSchool/BookShopSchool/Data/Models/Author.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopSchool.Data.Models
+{
+    public class Author
+    {
+        public Author()
+        {
+            this.AuthorsBooks = new HashSet<AuthorBook>();
+        }
+
+        public int Id {  get; set; }
+
+        [Required]
+        [MaxLength]
+
+        public string FirstName {  get; set; }
+        [Required] [MaxLength(30)][MinLength(3)]
+        public string LastName { get; set; }
+        [Required][MaxLength(30)][MinLength(3)]
+        public string Email { get; set; }
+        [Required]
+        public string Phone {  get; set; }
+        [Required]
+
+        public ICollection<AuthorBook> AuthorBooks { get; set; }
+    }
+}
---
+++ BookShopSchool/BookShopSchool/Data/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopSchool.Data.Models
+{
+    internal class Book
+    {
+        public Book()
+        {
+            this.AuthorsBooks = new HashSet<AuthorBook>();
+        }
+
+        public int id { get; set; }
+        [Required]
+
+
+
+
+    }
+}
