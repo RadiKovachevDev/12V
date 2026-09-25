79e343e6f9ef66d4f72d8cda212fe63c1b9e3d89
RadiKovachevDev <radi.kovachev.dev@gmail.com>
2026-09-25 10:28:32 +0300

    First Release

---
+++ BookShopSchool/BookShopSchool/Data/Models/Enums/Genre.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookShopSchool.Data.Models.Enums
+{
+    internal class Genre
+    {
+    }
+}
